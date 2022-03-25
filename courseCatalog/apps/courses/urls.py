b457ef6b9162186a31e962ee307456eb19bd28e8
Faik <faig.mammadov93@gmail.com>
2022-03-25 22:33:18 +0400

    Second Commit

--- courseCatalog/apps/courses/urls.py
+++ courseCatalog/apps/courses/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('all/', courseListView.as_view(), name = 'all'),
 	path('add', courseCreateView.as_view(), name = 'add'),
 	path('course/detail/<int:pk>/', courseDetailView.as_view(), name = 'detail'),
+	#path('selected-courses', courseListView.as_view(), name = 'selected')
 
 ]
 
